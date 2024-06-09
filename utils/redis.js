815a8a4822fc8d788db20d3adf01df865d4ed286
rizqikazukun <35159027+rizqikazukun@users.noreply.github.com>
2024-06-09 22:25:10 +0700

    fix redis ssl

--- utils/redis.js
+++ utils/redis.js
@@ -1,15 +1,15 @@
 // require("dotenv").config();
 const Redis = require("ioredis");
 
-let redis;
-
-redis = new Redis({
-  username: process.env.REDIS_USER, 
+const redis = new Redis({
+  username: process.env.REDIS_USER,
   host: process.env.REDIS_HOST,
-  password: process.env.REDIS_PASS, 
-  port: process.env.REDIS_PORT, 
+  password: process.env.REDIS_PASS,
+  port: process.env.REDIS_PORT,
   autoResubscribe: false,
   maxRetriesPerRequest: 5,
+  tls: true,
+  enableTLSForSentinelMode: false
 });
 
 module.exports = redis;
